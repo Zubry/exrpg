7c4de8bcd884b7c662c637f38810f3f020cab9e6
James Steele <jamessteele90@gmail.com>
2018-12-07 21:27:42 +0000

    Fix tabs

--- lib/skills/farming/farming.server.ex
+++ lib/skills/farming/farming.server.ex
@@ -1,19 +1,19 @@
 defmodule Farming do
-    use Script
-    
-    mount do
-        Farming.Patches.create
-    end
-    
-    on :plant, [ location, plant ] do
-        Farming.Model.plant(state, location, plant)
-    end
-    
-    on :harvest, [ location ] do
-        Farming.Model.harvest(state, location)
-    end
-    
-    on :check, [ location ] do
-        Farming.Model.check(state, location)
-    end
+  use Script
+  
+  mount do
+    Farming.Patches.create
+  end
+  
+  on :plant, [ location, plant ] do
+    Farming.Model.plant(state, location, plant)
+  end
+  
+  on :harvest, [ location ] do
+    Farming.Model.harvest(state, location)
+  end
+  
+  on :check, [ location ] do
+    Farming.Model.check(state, location)
+  end
 end
