a825285d0a50b7b6d5ca582ae8bc385077395185
James Steele <jamessteele90@gmail.com>
2018-12-07 21:46:33 +0000

    Create skeleton for the inventory

---
+++ lib/interfaces/inventory/inventory.server.ex
@@ -0,0 +1,27 @@
+defmodule Inventory do
+    use Script
+    
+    mount do
+        %{}
+    end
+    
+    on :add, [item, quantity] do
+        
+    end
+    
+    on :remove, [item, quantity] do
+    
+    end
+    
+    on :has, [item] do
+    
+    end
+    
+    on :weight, [] do
+    
+    end
+    
+    on :encumbered?, [] do
+    
+    end
+end
